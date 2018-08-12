cfd6a4d7365c87a6c1e097b3838b33bc617238ca
Renan Machado <RSilva41@slb.com>
2018-08-12 11:52:47 -0500

    Add edit menu item

--- vagrant/restaurant-menu/project.py
+++ vagrant/restaurant-menu/project.py
@@ -30,7 +30,8 @@ def restaurantMenu(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
     if request.method == 'POST':
-        newMenuItem = MenuItem(name=request.form['menuItemName'], restaurant_id=restaurant_id)
+        newMenuItem = MenuItem(
+            name=request.form['menuItemName'], restaurant_id=restaurant_id)
         session.add(newMenuItem)
         session.commit()
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
@@ -40,9 +41,16 @@ def newMenuItem(restaurant_id):
 # Task 2: Create route for editMenuItem function here
 
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+    menuItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        menuItem.name = request.form['menuItemName']
+        session.add(menuItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('editMenuItem.html', menu_item=menuItem)
 
 # Task 3: Create a route for deleteMenuItem function here
 
