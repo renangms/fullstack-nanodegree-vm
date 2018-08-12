303151728c42db53c1415222f4aa5165a7176692
Renan Machado <RSilva41@slb.com>
2018-08-12 11:17:55 -0500

    Added new menu item using Form Requests and Redirects

--- vagrant/restaurant-menu/project.py
+++ vagrant/restaurant-menu/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Restaurant, Base, MenuItem
@@ -27,9 +27,15 @@ def restaurantMenu(restaurant_id):
 # Task 1: Create route for newMenuItem function here
 
 
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newMenuItem = MenuItem(name=request.form['menuItemName'], restaurant_id=restaurant_id)
+        session.add(newMenuItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('newMenuItem.html', restaurant_id=restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
